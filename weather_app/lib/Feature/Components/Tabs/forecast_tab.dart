f62856f5f21f6547d9b0778a511f746dce119afa
MansurSarkhanov <sarxanov.mansur@gmail.com>
2024-02-04 05:22:31 +0400

    test

---
+++ weather_app/lib/Feature/Components/Tabs/forecast_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ForecastTab extends StatelessWidget {
+  const ForecastTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
